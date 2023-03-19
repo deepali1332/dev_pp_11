5b611be3741f8b5f6b29c933cbd335708a7699bb
Deepali <the.yadavdeepali2710@gmail.com>
2023-03-20 00:19:28 +0530

    added intro.js

--- Module1/lec1/intro.js
+++ Module1/lec1/intro.js
@@ -1,1 +1,15 @@
-console.log("Hello world");
+function getfirstname(fullName)
+{
+    fullName = fullName.split(" ");
+    return fullName[0];
+}
+function getlastname(fullName){
+    fullName = fullName.split(" ");
+    return fullName[1];
+}
+function fun(fullName , cb){
+    let name = cb(fullName);
+    console.log(name);
+}
+fun("steve Man", getfirstname);
+fun("Steve Iron", getlastname);
