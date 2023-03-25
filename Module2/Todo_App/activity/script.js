07dc6e695321cd924ebd901b3766d3263ccd17ba
Deepali <the.yadavdeepali2710@gmail.com>
2023-03-25 23:35:06 +0530

    Css box model

--- Module2/Todo_App/activity/script.js
+++ Module2/Todo_App/activity/script.js
@@ -1,38 +1,54 @@
-let addTodoButton = document.querySelector(".add-todo");
-let todoInput = document.querySelector(".todo-input");
-let todoList = document.querySelector(".todo-list-container");
+let addTodoButton=document.querySelector(".add-todo");
+let todoInput=document.querySelector(".todo-input");
+let todosList=document.querySelector(".todos-list-containers");
 
-todoInput.addEventListener("keypress", function(e){
-    if(e.key == "Enter"){
-        addTodo();
-
+// attach click event on addTodoButton
+todoInput.addEventListener("keypress",function(e){
+    if(e.key=="Enter")
+    {
+        addToDo();
     }
 });
-addTodoButton.addEventListener("click", function(){
-    addTodo();
+addTodoButton.addEventListener("click",function() {
+    addToDo();    
 });
 
-function addTodo(e){
-    let todoInputValue = todoInput.value;
-    if(todoInputValue){
-        console.log(todoInputValue);
-        todoInput.value = "";
+
+function addToDo() {
+    let todoInputValue=todoInput.value;
+    if(todoInputValue)
+    {   appendTodo(todoInputValue);
+        //console.log(todoInputValue);
+        // it will empty the todoInput
+        todoInput.value="";
     }
 }
-function appendTodo(todo){
-    let todoItemDiv = document.createdElement("div");
-    todoItemDive.classList.add("todo-item");
-    let pTag = document.createElement("p");
+
+
+
+function appendTodo(todo) {
+    let todoItemDiv=document.createElement("div");
+    todoItemDiv.classList.add("todo-item");
+    //  <div class="todo-item"> </div>
+
+    let pTag=document.createElement("p");
     pTag.classList.add("todo-input");
-    pTag.textContent = todo;
-    let deleteTodoButton = document.createElement("button");
-    deleteTodoButton.classList.add("delete-todo");
-    delteteTodoButton.textContent = "Delete";
-    console.log(todoItemDive);
+    pTag.textContent=todo;
+
+    // <p class="todo-input">Learn Css</p>
+
+    let DeleteTodoButton=document.createElement("button");
+    DeleteTodoButton.classList.add("delete-todo");
+    DeleteTodoButton.textContent="Delete";
+
+    // <button class="delete-todo">Delete</button>
+    DeleteTodoButton.addEventListener("click",deleteTodo);
 
     todoItemDiv.append(pTag);
-    console.log(todoItemDiv);
+    todoItemDiv.append(DeleteTodoButton);
+    todosList.append(todoItemDiv);
+}
 
-    todoItemDiv.append(deleteTodoButton);
-    console.log(todoItemDiv);
+function deleteTodo(e) {
+ e.target.parentNode.remove();
 }
